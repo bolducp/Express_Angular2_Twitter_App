c5a63727c9b5371aecbeaf862996557ebdac4226
Paige Bolduc <paigebolduc@gmail.com>
2016-06-18 12:32:07 -0400

    remove unecessary serve favicon code

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ require('dotenv').config();
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
