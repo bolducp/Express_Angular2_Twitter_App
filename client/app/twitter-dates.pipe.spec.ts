c4bd82beb315d97537606d38dd827806d7fdba6b
Paige Bolduc <paigebolduc@gmail.com>
2016-10-17 20:33:48 -0400

    Dockerize

--- client/app/twitter-dates.pipe.spec.ts
+++ client/app/twitter-dates.pipe.spec.ts
@@ -1,27 +1,25 @@
 import { TwitterDatePipe, TwitterDateWithTimePipe } from './twitter-dates.pipe';
 
 describe('TwitterDatePipe', () => {
-  let pipe: TwitterDatePipe;
-
   beforeEach(() => {
-    pipe = new TwitterDatePipe();
+    this.pipe = new TwitterDatePipe();
   });
 
   it('strips out the time and timezone data', () => {
-    expect(pipe.transform('Tue Dec 23 04:03:33 +0000 2008')).toEqual('Tue Dec 23 2008');
+    expect(this.pipe.transform('Tue Dec 23 040333 +0000 2008')).toEqual('Tue Dec 23 2008');
   });
 
   describe('if date in unexpected format, returns "Not Available"', () => {
     it('date string not 6 words long', () => {
-      expect(pipe.transform('Dec 23 040333 +0000 2008')).toEqual('Not Available');
+      expect(this.pipe.transform('Dec 23 040333 +0000 2008')).toEqual('Not Available');
     });
 
     it('forth word is not a time stamp', () => {
-      expect(pipe.transform('Tue Dec 23 +0000 2008')).toEqual('Not Available');
+      expect(this.pipe.transform('Tue Dec 23 +0000 2008')).toEqual('Not Available');
     });
 
     it('fifth word is not a timezone', () => {
-      expect(pipe.transform('Tue Dec 23 04:03:33 2008')).toEqual('Not Available');
+      expect(this.pipe.transform('Tue Dec 23 04:03:33 2008')).toEqual('Not Available');
     });
   });
 });
